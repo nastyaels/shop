8459a78bb3a938525257ba3df785adef6fbabb10
a <a>
2021-11-05 23:01:46 +0200

    cake

---
+++ src/main/java/com/eliseeva/shop/goods/CakeRepository.java
@@ -0,0 +1,10 @@
+package com.eliseeva.shop.goods;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface CakeRepository extends JpaRepository<CakeEntity,Long> {
+
+    boolean existsByName(String name);
+
+
+}
